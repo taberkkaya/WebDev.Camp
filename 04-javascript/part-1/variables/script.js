0ded8625629a3dcf781ec865f1d979f794d49deb
akaya <taberkkaya@gmail.com>
2024-11-21 08:41:26 +0300

    variables js

---
+++ 04-javascript/part-1/variables/script.js
@@ -0,0 +1,20 @@
+var a = 5500;
+var b = 5000;
+var c = 6000;
+var d = 6000;
+var kdv = 1.18;
+
+var a = 6000;
+
+console.log(a * kdv); //product a
+
+console.log(a); //product a
+
+console.log(b * kdv); //product b
+console.log(c * kdv); //product c
+console.log(d * kdv); //product d
+
+var price = 5000;
+
+var productName = "S24";
+var productName2 = "S22";
