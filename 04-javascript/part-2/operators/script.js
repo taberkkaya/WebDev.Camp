ecdb97e18e279a0d964cced63124b5166823f418
akaya <taberkkaya@gmail.com>
2024-11-21 17:16:57 +0300

    part#2 start

---
+++ 04-javascript/part-2/operators/script.js
@@ -0,0 +1,35 @@
+// The Assignment Operator = assigns values
+let result;
+let a = 10,
+  b = 2,
+  c = 4;
+// Arithmetic Operators
+
+result = a + b;
+result = a - b;
+result = a * b;
+result = a / b;
+result = a % b;
+// result = a++; // a = a + 1
+// result = ++a;
+
+// Assignment Operators
+
+result = a;
+result += a + b;
+result += a;
+result -= a;
+result /= a;
+result *= a;
+result %= a;
+
+// Comparison Operators
+
+result = a == b;
+result = a == b;
+result = a != b;
+result = 2 == "2";
+result = 2 === "2";
+result = a >= b;
+//
+console.log(result);
