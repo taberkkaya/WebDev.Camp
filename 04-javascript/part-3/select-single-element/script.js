8f7701f5526a232b42e5f4fd24aff30838f6d0fc
akaya <taberkkaya@gmail.com>
2024-11-21 18:47:16 +0300

    querySelector

---
+++ 04-javascript/part-3/select-single-element/script.js
@@ -0,0 +1,26 @@
+let result;
+
+// result = document.getElementById("title");
+// result = document.getElementById("title").id;
+// result = document.getElementById("title").className;
+// result = document.getElementById("title").classList;
+// result = document.getElementById("title").style.fontSize = "40px";
+// result = document.getElementById("title").style.color = "red";
+// // result = document.getElementById("title").style.display = "none";
+// result = document.getElementById("title").innerText = "Shopping";
+// result = document.getElementById("title").innerHTML = "<p>Hello</p>";
+
+// result = document.querySelector("#title");
+// result = document.querySelector(".text-center");
+// result = document.querySelector("h1");
+// result = document.querySelector("input[type='submit']");
+// result = document.querySelector("li:nth-child(2)");
+// result = document.querySelector("li:nth-child(2)").innerText = "Item 2 !";
+
+const parentElement = document.querySelector("ul");
+console.log(parentElement);
+
+const firstElement = parentElement.querySelector("li");
+console.log(firstElement);
+
+console.log(result);
