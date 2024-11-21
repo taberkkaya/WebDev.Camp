dde293acf84f18d4666dadd1f734086fb45be7d9
akaya <taberkkaya@gmail.com>
2024-11-21 17:46:30 +0300

    part-2 skipped

---
+++ 04-javascript/part-2/conditions/script.js
@@ -0,0 +1,12 @@
+let username = "taberkkaya";
+let condition = username == "taberkkaya";
+
+if (condition) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
+if (!condition) {
+  console.log("No");
+}
