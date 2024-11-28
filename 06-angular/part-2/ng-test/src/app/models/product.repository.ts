3c0913b3cdde7b7dc8a5bc6a02f80ac8d5c6fa12
akaya <taberkkaya@gmail.com>
2024-11-28 18:26:59 +0300

    angular models,ng-template

--- 06-angular/part-2/ng-test/src/app/models/product.repository.ts
+++ 06-angular/part-2/ng-test/src/app/models/product.repository.ts
@@ -1,24 +1,7 @@
-import { Component } from '@angular/core';
+import { Product } from './product';
 
-@Component({
-  selector: 'app-product-list',
-  standalone: false,
-
-  templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css',
-})
-export class ProductListComponent {
-  product = {
-    id: 1,
-    name: 'Iphone 3',
-    price: 20000,
-    imgUrl: 'https://picsum.photos/id/237/200/300',
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. tenetur eos perferendis laudantium cumque similique minus exercitationem aliquid deserunt!',
-    isActive: false,
-  };
-
-  productList = ['Lorem', 'lorem', 'lore.'];
-  products: any = [
+export class ProductRepository {
+  private products: Product[] = [
     {
       id: 1,
       name: 'Iphone',
@@ -33,7 +16,7 @@ export class ProductListComponent {
       price: 20000,
       imgUrl: 'https://picsum.photos/id/237/200/300',
       desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. tenetur eos perferendis laudantium cumque similique minus exercitationem aliquid deserunt!',
-      isActive: false,
+      isActive: true,
     },
     {
       id: 1,
@@ -44,4 +27,12 @@ export class ProductListComponent {
       isActive: true,
     },
   ];
+
+  getProducts(): Product[] {
+    return this.products.filter((p) => p.isActive);
+  }
+
+  getProductById(id: number): Product | undefined {
+    return this.products.find((p) => p.id == id);
+  }
 }
---
+++ 06-angular/part-2/ng-test/src/app/models/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+  desc: string;
+  isActive: boolean;
+}
