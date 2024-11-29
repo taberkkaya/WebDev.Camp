18dc9c96969d7c331f8df71e21c354f51717e3c3
akaya <taberkkaya@gmail.com>
2024-11-29 08:19:25 +0300

    synced by course

--- 06-angular/part-2/ng-app/src/app/category-list/category-list.component.ts
+++ 06-angular/part-2/ng-app/src/app/category-list/category-list.component.ts
@@ -10,10 +10,23 @@ import { Category } from '../models/category';
 })
 export class CategoryListComponent {
   categories: Category[];
+  selectedCategory: Category | null;
   categoryRepository: CategoryRepository;
 
   constructor() {
     this.categoryRepository = new CategoryRepository();
     this.categories = this.categoryRepository.getCategories();
+  }
+
+  selectCategory(category: Category) {
+    if (this.selectedCategory) {
+      if (this.selectedCategory != category) {
+        this.selectedCategory = category;
+      } else {
+        this.selectedCategory = null;
+      }
+    } else {
+      this.selectedCategory = category;
+    }
   }
 }
