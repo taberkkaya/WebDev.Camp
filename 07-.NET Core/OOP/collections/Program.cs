0b3f069b21f6b892b7fca345770f12c4bceb6b13
taberkkaya <taberkkaya@gmail.com>
2024-12-09 18:19:28 +0300

    c# section done

---
+++ 07-.NET Core/OOP/collections/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Array => fixed size
+//  Collection
+// non-generic
+// ArrayList, SortedList
+// int,string,Product
+
+// ArrayList myList = new ArrayList();
+
+
+using System.Collections;
+
+ArrayList myList = new ArrayList();   
+
+myList.Add(10);
+myList.Add("10");
+myList.Add("Abc");
+myList.Add(10.5);
+
+
+ArrayList myList2 = new ArrayList(){10,"10","Abc",10.5};
+
+myList2.Insert(0,"0");
+
+myList.AddRange(myList2);
+
+foreach(var item in myList2){
+    Console.WriteLine(item);    
+}
+
+ Console.WriteLine("************************");
+
+ foreach(var item in myList){
+    Console.WriteLine(item);    
+}
--- 07-.NET Core/console-app/app/obj/Debug/net9.0/app.AssemblyInfo.cs
+++ 07-.NET Core/console-app/app/obj/Debug/net9.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5231e8d0da910daba7eb827995568ca145f639e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6779b00c0fb46ff3ecca3a04255f03e2e5f9a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
