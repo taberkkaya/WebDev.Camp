d5231e8d0da910daba7eb827995568ca145f639e
taberkkaya <taberkkaya@gmail.com>
2024-12-09 11:24:27 +0300

    some sections skipped

---
+++ 07-.NET Core/OOP/func/Program.cs
@@ -0,0 +1,16 @@
+﻿var p1 = new Person{Name = "Name", Year = 2022};
+var p2 = new Person{Name = "Name2", Year = 1999};
+var p3 = new Person{Name = "Name3", Year = 2001};
+
+p1.Intro();
+p2.Intro();
+p3.Intro();
+
+class Person {
+    public string Name { get; set; }
+    public int Year { get; set; }
+
+    public void Intro(){
+        Console.WriteLine($"Name: {this.Name}, Age: {2024-this.Year}");
+    }
+}
